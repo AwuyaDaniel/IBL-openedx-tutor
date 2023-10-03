b905f9dbc2e765b3cb570daa8facb36253cc62ec
awuyadaniel <awuyadanielz@gmail.com>
2023-10-03 23:27:57 +0100

    bug fix

--- tutoribl_openedx_tutor/patches/openedx-common-settings.py
+++ tutoribl_openedx_tutor/patches/openedx-common-settings.py
@@ -1,3 +1,3 @@
-from django.conf.global_settings import INSTALLED_APPS
-
-INSTALLED_APPS.append('tutoribl_openedx_tutor')
+# from django.conf.global_settings import INSTALLED_APPS
+#
+# INSTALLED_APPS.append('tutoribl_openedx_tutor')
