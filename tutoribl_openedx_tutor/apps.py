cc24b31a6f541c168ac2b4bbb5dce56723283ea9
awuyadaniel <awuyadanielz@gmail.com>
2023-10-04 15:01:54 +0100

    bug fix

--- tutoribl_openedx_tutor/apps.py
+++ tutoribl_openedx_tutor/apps.py
@@ -6,108 +6,48 @@ from django.apps import AppConfig
 from edx_django_utils.plugins.constants import (
     PluginURLs, PluginSettings, PluginContexts
 )
+from openedx.core.djangoapps.plugins.constants import (
+    PluginURLs,
+    PluginSettings,
+    ProjectType,
+    SettingsType,
+)
 # from openedx.core.djangoapps.plugins.constants import PluginURLs
 
 class IblOpenedxAppConfig(AppConfig):
     """
     Configuration for the IBL_openedx_app Django application.
     """
+    default_auto_field = "django.db.models.BigAutoField"
 
-#     name = 'IBL_openedx_app'
-# class MyAppConfig(AppConfig):
-    name = 'tutoribl_openedx_tutor'
-
-    # Class attribute that configures and enables this app as a Plugin App.
+    name = 'IBL_openedx_app'
     plugin_app = {
-
-        # Configuration setting for Plugin URLs for this app.
+        # mcdaniel oct-2021
+        # this is how you inject a python list of urls into lms.urls.py
+        #
+        # The three dict attributes literally equate to the following
+        # lines of code being injected into edx-platform/lms/urls.py:
+        #
+        # import openedx_plugin_cms.urls.py
+        # url(r"^openedx_plugin/cms", include((urls, "openedx_plugin_cms"), namespace="openedx_plugin_cms")),
         PluginURLs.CONFIG: {
-
-            # Configure the Plugin URLs for each project type, as needed. The full list of project types for edx-platform is
-            # here:
-            # https://github.com/openedx/edx-platform/blob/2dc79bcab42dafed2c122eb808cdd5604327c890/openedx/core/djangoapps/plugins/constants.py#L14 .
-            # Other IDAs may use different values.
-            'lms.djangoapp': {
-
-                # The namespace to provide to django's urls.include.
-                PluginURLs.NAMESPACE: 'IBL_Openedx',
-
-                # The application namespace to provide to django's urls.include.
-                # Optional; Defaults to None.
-                PluginURLs.APP_NAME: 'tutoribl_openedx_tutor',
-
-                # The regex to provide to django's urls.url.
-                # Optional; Defaults to r''.
-                PluginURLs.REGEX: r'^api/tutoribl_openedx_tutor/',
-
-                # The python path (relative to this app) to the URLs module to be plugged into the project.
-                # Optional; Defaults to 'urls'.
-                PluginURLs.RELATIVE_PATH: 'urls',
+            ProjectType.CMS: {
+                PluginURLs.NAMESPACE: name,
+                PluginURLs.REGEX: "^IBL/lms/",
+                PluginURLs.RELATIVE_PATH: "urls",
             }
         },
-
-        # Configuration setting for Plugin Settings for this app.
-        # PluginSettings.CONFIG: {
+        # mcdaniel oct-2021
+        # this is how you inject settings into lms.envs.common.py and lms.envs.production.py
+        # relative_path == a python module in this repo
         #
-        #     # Configure the Plugin Settings for each Project Type, as needed. The full list of setting types for edx-platform is
-        #     # here:
-        #     # https://github.com/openedx/edx-platform/blob/2dc79bcab42dafed2c122eb808cdd5604327c890/openedx/core/djangoapps/plugins/constants.py#L25 .
-        #     # Other IDAs may use different values.
-        #     'lms.djangoapp': {
-        #
-        #         # Configure each settings, as needed.
-        #         'production': {
-        #
-        #             # The python path (relative to this app) to the settings module for the relevant Project Type and Settings Type.
-        #             # Optional; Defaults to 'settings'.
-        #             PluginSettings.RELATIVE_PATH: 'tutoribl_openedx_tutor.settings.production',
-        #         },
-        #         'common': {
-        #             PluginSettings.RELATIVE_PATH: 'tutoribl_openedx_tutor.settings.common',
-        #         },
-        #     }
-        # },
-
-        # Configuration setting for Plugin Signals for this app.
-        # PluginSignals.CONFIG: {
-        #
-        #     # Configure the Plugin Signals for each Project Type, as needed.
-        #     'lms.djangoapp': {
-        #
-        #         # The python path (relative to this app) to the Signals module containing this app's Signal receivers.
-        #         # Optional; Defaults to 'signals'.
-        #         PluginSignals.RELATIVE_PATH: 'my_signals',
-        #
-        #         # List of all plugin Signal receivers for this app and project type.
-        #         PluginSignals.RECEIVERS: [{
-        #
-        #             # The name of the app's signal receiver function.
-        #             PluginSignals.RECEIVER_FUNC_NAME: 'on_signal_x',
-        #
-        #             # The full path to the module where the signal is defined.
-        #             PluginSignals.SIGNAL_PATH: 'full_path_to_signal_x_module.SignalX',
-        #
-        #             # The value for dispatch_uid to pass to Signal.connect to prevent duplicate signals.
-        #             # Optional; Defaults to full path to the signal's receiver function.
-        #             PluginSignals.DISPATCH_UID: 'my_app.my_signals.on_signal_x',
-        #
-        #             # The full path to a sender (if connecting to a specific sender) to be passed to Signal.connect.
-        #             # Optional; Defaults to None.
-        #             PluginSignals.SENDER_PATH: 'full_path_to_sender_app.ModelZ',
-        #         }],
-        #     }
-        # },
-
-        # Configuration setting for Plugin Contexts for this app.
-        # PluginContexts.CONFIG: {
-        #
-        #     # Configure the Plugin Signals for each Project Type, as needed.
-        #     'lms.djangoapp': {
-        #
-        #         # Key is the view that the app wishes to add context to and the value
-        #         # is the function within the app that will return additional context
-        #         # when called with the original context
-        #         'course_dashboard': 'my_app.context_api.get_dashboard_context'
-        #     }
-        # }
+        # This dict causes all constants defined in this settings/common.py and settings.production.py
+        # to be injected into edx-platform/lms/envs/common.py and edx-platform/lms/envs/production.py
+        # Refer to settings/common.py and settings.production.py for example implementation patterns.
+        PluginSettings.CONFIG: {
+            ProjectType.CMS: {
+                SettingsType.PRODUCTION: {PluginSettings.RELATIVE_PATH: "settings.production"},
+                SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: "settings.common"},
+            }
+        },
     }
